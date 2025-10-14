23391b9684f907172bb7abcdf3070b46e9467898
Daniel <moidossantos01@gmail.com>
2025-10-14 19:10:25 +0400

    test v3

--- test/test_bubble.sh
+++ test/test_bubble.sh
@@ -9,7 +9,7 @@ make clean && make all
 # on execute le programme et redirige la sortie vers un fichier
 ./bubble_sort > output.txt
 
-attendu = '=== Tests de tri avec différents types et ordres ===
+attendu='=== Tests de tri avec différents types et ordres ===
 
 1. Tri d entiers:
 Tableau original: 64 34 25 12 22 11 90 
@@ -27,10 +27,14 @@ Tri alphanumérique croissant: "item1" "item2" "item3" "item10" "item20"
 Tri alphanumérique décroissant: "item20" "item10" "item3" "item2" "item1" '
 
 # on verifie si la sortie contient le texte attendu
-if [ "$(cat output.txt)" == "$attendu" ]; then
+if [ "$(cat output.txt)" = "$attendu" ]; then
   echo "Test réussi"
   exit 0
 else
   echo "Test échoué"
+  echo "=== Sortie obtenue ==="
+  cat output.txt
+  echo "=== Sortie attendue ==="
+  echo "$attendu"
   exit 1
 fi
